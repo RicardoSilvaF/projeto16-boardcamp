5bfecdc25de09515ef4c96403401a3ff93fb3939
RicardoSilvaF <zanphs@gmail.com>
2023-05-15 10:12:25 -0300

    Correcao dos package

--- src/app.js
+++ src/app.js
@@ -1,13 +1,12 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
 import router from "./routers/routes.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(router);
-dotenv.config();
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`servidor ligado na porta ${PORT}`));
